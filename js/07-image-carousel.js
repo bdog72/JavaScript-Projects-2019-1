cf3610566de3bf199b01f4e5b008c56fd19c549b
Brian Bycynski <high5bri@hotmail.com>
2019-07-04 06:07:40 -0400

    Finished section 33

---
+++ js/07-image-carousel.js
@@ -0,0 +1,73 @@
+// //
+// //
+// //
+// let slideIndex = 0;
+// let timer;
+// const myImages = [
+//   {
+//     img: 'https://via.placeholder.com/250/09f?text=image1',
+//     caption: 'Hello World Image 1'
+//   },
+//   {
+//     img: 'https://via.placeholder.com/250/e4e?text=image2',
+//     caption: 'Hello World Image 2'
+//   },
+//   {
+//     img: 'https://via.placeholder.com/250/1f1?text=image3',
+//     caption: 'Hello World Image 3'
+//   }
+// ];
+
+// builder();
+
+// function builder() {
+//   for (let x = 0; x < myImages.length; x++) {
+//     let slide = document.createElement('div');
+//     slide.setAttribute('class', 'mySlide fade');
+//     let img = document.createElement('img');
+//     img.setAttribute('src', myImages[x].img);
+//     let cap = document.createElement('div');
+//     cap.classList.add('caption');
+//     cap.innerText = myImages[x].caption;
+//     slide.appendChild(img);
+//     slide.appendChild(cap);
+//     document.querySelector('.slideContainer').appendChild(slide);
+//     console.log(slide);
+//   }
+// }
+
+let slideIndex = 0;
+let timer;
+const myImages = [
+  {
+    img: 'https://via.placeholder.com/250/09f?text=image1',
+    caption: 'Hello World Image 1'
+  },
+  {
+    img: 'https://via.placeholder.com/250/e4e?text=image2',
+    caption: 'Hello World Image 2'
+  },
+  {
+    img: 'https://via.placeholder.com/250/1f1?text=image3',
+    caption: 'Hello World Image 3'
+  }
+];
+builder();
+
+function builder() {
+  for (let x = 0; x < myImages.length; x++) {
+    let slide = document.createElement('div');
+    slide.setAttribute('class', 'mySlide fade');
+    let img = document.createElement('img');
+    img.setAttribute('src', myImages[x].img);
+    let cap = document.createElement('div');
+    cap.classList.add('caption');
+    cap.innerText = myImages[x].caption;
+    slide.appendChild(img);
+    slide.appendChild(cap);
+    document.querySelector('.slideContainer').appendChild(slide);
+    console.log(slide);
+  }
+
+  console.log(myImages);
+}
